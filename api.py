2a2df0ac1e189cc89f68c2820a2a17a6fe7e55ac
Yifeng <hyf94244280@gmail.com>
2018-12-22 18:32:32 +0800

    Transform ideas to concise ideas
    [#162824095]

--- api.py
+++ api.py
@@ -12,9 +12,23 @@ class IdeaList(Resource):
     def get(self):
         r = session.get('https://www.mindmeister.com/services/rest?api_key=da85a513e81709500df4387d1fb2c9bf&auth_token=ZAj1q3RiBlr3Xy7CwaIU&map_id=1193541820&method=mm.maps.getMap&response_format=xml&api_sig=b9fd158ee1e9200074710c87dbbb0cf1')
         rsp = r.xml.xpath('//rsp', first=True)
-        return json.loads(XML(xml=rsp.xml).json())
+        rspJson = json.loads(XML(xml=rsp.xml).json())['rsp']
+
+        map_ideas = {}
+        map_ideas['mapId'] = rspJson['map']['@id']
+        map_ideas['ideas'] = [self.transform_idea(idea) for idea in rspJson['ideas']['idea']]
+
+        return map_ideas
+
+    def transform_idea(self, idea):
+        concise_idea = {
+            'id': idea['id']['$'],
+            'title': idea['title']['$'],
+            'parent': idea['parent']['$'] if idea['parent'] else ''
+        }
+        return concise_idea
 
 api.add_resource(IdeaList, '/ideas')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
