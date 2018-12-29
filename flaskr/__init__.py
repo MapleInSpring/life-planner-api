cc82eaa823b75f9309219dd76ba293e519446ab8
Yifeng <hyf94244280@gmail.com>
2018-12-29 17:09:22 +0800

    Add get all actions endpoint
    [#162870842]

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from .idea_list_resource import IdeaList
+from .action_list_resource import ActionList
 
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ api = Api(app)
 
 
 api.add_resource(IdeaList, '/ideas')
+api.add_resource(ActionList, '/actions')
 
 if __name__ == '__main__':
     app.run(port=8080)
---
+++ flaskr/action_list_resource.py
@@ -0,0 +1,13 @@
+from flask_restful import Resource
+from .mind_client import MindClient
+from .models.idea_category import IdeaCategory
+from .transformer_dto import TransformerDTO
+
+
+class ActionList(Resource):
+    def get(self):
+        idea_list = MindClient.get_all_ideas()
+
+        actions = [idea for idea in idea_list if idea['category'] == IdeaCategory.ACTION]
+
+        return TransformerDTO.transform_idea_list_dto(actions)
